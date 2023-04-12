defd8627ee555453c07bfeb6ab8c22f2a0be55b9
Isaac López Nolasco <munoza928@gmail.com>
2023-04-12 15:15:15 -0600

    main_avance
    subi mi rpyecto casi acabado

---
+++ Enemy IA System/Assets/Sistema de ia basica/Scripts/EnemigoAI.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class EnemigoAI : MonoBehaviour
+{
+    public Transform jugador;
+    private NavMeshAgent agente;
+    private float distanciaMinima = 20.0f; // establece una distancia para detectar al jugador
+
+    private void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+        ObtenerNuevoDestino(); 
+    }
+
+    private void Update()
+    {
+        // verifica a que distancia esta el jugador
+        float distanciaJugador = Vector3.Distance(transform.position, jugador.position);
+
+      
+        if (distanciaJugador < distanciaMinima)
+        {
+            // el Raycast no sirve para detectar al jugador
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, jugador.position - transform.position, out hit, distanciaMinima))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    // Si se detecta al jugador lo empezara a perseguir
+                    agente.destination = jugador.position;
+                }
+            }
+        }
+        else
+        {
+            // Si no está persiguiendo al jugador se movera solo de nuevo
+            if (!agente.hasPath || agente.remainingDistance < 0.5f)
+            {
+                // Si ya no tiene un destino al cual llegar obtiene uno nuevo
+                ObtenerNuevoDestino();
+            }
+        }
+    }
+
+    private void ObtenerNuevoDestino()
+    {
+        
+        Vector3 destino = Random.insideUnitSphere * 20.0f + transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(destino, out hit, 20.0f, NavMesh.AllAreas);
+
+        // Establecer la nueva posición como objetivo del NavMeshAgent
+        agente.destination = hit.position;
+    }
+}
+
+
---
+++ Enemy IA System/Assets/Sistema de ia basica/Scripts/Player.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    Transform cam;
+    CharacterController control;
+
+    public float speedCam;
+    private float camRotation = 0f;
+    public float speed;
+    public float sprintSpeed;
+    public float gravityForce;
+    private float gravityMove = 0f;
+    public float jumpForce;
+
+    public TextMeshProUGUI countText;
+    private int count = 0;
+    private int dead = 0;
+    public GameObject WinText;
+
+    
+    private bool HActivo;
+    private bool VActivo;
+
+    private void Start()
+    {
+        cam = transform.GetChild(0).GetComponent<Transform>();
+        control = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(new Vector3(0, mouseX, 0) * speedCam * Time.deltaTime);
+
+        camRotation -= mouseY * speedCam * Time.deltaTime;
+        camRotation = Mathf.Clamp(camRotation, -90, 90);
+        cam.localRotation = Quaternion.Euler(new Vector3(camRotation, 0, 0));
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftShift) && (moveX != 0 || moveZ != 0))
+        {
+            movement = (transform.right * moveX + transform.forward * moveZ) * sprintSpeed * Time.deltaTime;
+        }
+        else
+        {
+            movement = (transform.right * moveX + transform.forward * moveZ) * speed * Time.deltaTime;
+        }
+
+            control.Move(movement);
+
+        control.Move(new Vector3(0, gravityMove, 0) * Time.deltaTime);
+
+        if(!control.isGrounded)
+        {
+            gravityMove += gravityForce;
+        }
+        else
+        {
+            gravityMove = 0f;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && control.isGrounded)
+        {
+            gravityMove = jumpForce;
+        }
+
+        if(Input.GetButtonDown("Horizontal")) //Reproduce audio de pisadas
+        {
+            if(VActivo == false)
+            {
+                HActivo = true;
+               
+            }
+            
+        }
+        if(Input.GetButtonDown("Vertical"))
+        {
+            if(HActivo == false)
+            {
+                VActivo = true;
+               
+            }
+            
+        }
+
+        if(Input.GetButtonUp("Horizontal"))
+        {
+            HActivo = false;
+           
+            
+        }
+        if (Input.GetButtonUp("Vertical"))
+        {
+            VActivo = false;
+           
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.CompareTag("punto"))
+        {
+            count++;
+            
+            other.gameObject.SetActive(false);
+            if (count == 113)
+            {
+                WinText.SetActive(true);
+            }
+        }
+
+    }
+
+}
+
